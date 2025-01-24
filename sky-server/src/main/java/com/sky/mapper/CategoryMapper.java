33f35782837ff7c4f4023446c03639fc1c6de069
simmons <sunhuaxiao2@gmail.com>
2025-01-24 12:24:49 +0800

    新增分类业务代码开发、 修改分类业务代码开发、 分类分页查询业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,35 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface CategoryMapper {
+
+    /**
+     * 新增分类
+     * @param category
+     */
+    @Insert("insert into category (id, type, name, sort, status, create_time, update_time, create_user, update_user) " +
+            "values (#{id}, #{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    public void insert(Category category);
+
+
+    /**
+     * 修改分类
+     * @param category
+     */
+    void update(Category category);
+
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+}
