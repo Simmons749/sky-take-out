59365e9cbd311d822922e685f5e6dd45107cd345
simmons <sunhuaxiao2@gmail.com>
2025-02-01 14:35:05 +0800

    用户下单业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -2,10 +2,12 @@ package com.sky.controller.user;
 
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
+import com.sky.service.OrderService;
 import com.sky.vo.OrderSubmitVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user/order")
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
     /**
      * 用户下单
      * @param ordersSubmitDTO
@@ -25,8 +30,11 @@ public class OrderController {
     @ApiOperation("用户下单")
     @PostMapping("/submit")
     public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
+        log.info("用户下单, {}", ordersSubmitDTO);
 
-        return null;
+        OrderSubmitVO orderSubmitVO = orderService.submit(ordersSubmitDTO);
+
+        return Result.success(orderSubmitVO);
     }
 
 }
