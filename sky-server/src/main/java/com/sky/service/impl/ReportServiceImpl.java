9270b27206fefb60cdc5889e9c32ac0d66ac36dc
simmons <sunhuaxiao2@gmail.com>
2025-02-04 11:14:25 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,8 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
 
 
     /**
@@ -63,6 +67,57 @@ public class ReportServiceImpl implements ReportService {
                 .builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+
+    /**
+     * 统计指定时间区间内用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        // 当前集合用于将从begin开始到end结束中的所有日期都收集
+        List<LocalDate> dateList = new ArrayList<>();
+
+        // 查询开始日期前的用户数量
+        LocalDateTime endTime = LocalDateTime.of(begin, LocalTime.MIN);
+        Map map = new HashMap();
+        map.put("end", endTime);
+        Integer userTotal = userMapper.countByMap(map);
+        userTotal = userTotal == null ? 0 : userTotal;
+
+        dateList.add(begin);
+        while (!begin.isEqual(end)) {
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        // 用户累加量和用户新增
+        List<Integer> userTotalList = new ArrayList<>();
+        List<Integer> userDailyList = new ArrayList<>();
+
+
+
+        for (LocalDate date : dateList) {
+            // 查询date日期对应的用户数据
+            LocalDateTime beginDaily = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endDaily = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map_daily = new HashMap();
+            map_daily.put("begin", beginDaily);
+            map_daily.put("end", endDaily);
+            Integer new_user = userMapper.countByMap(map_daily);
+            new_user = new_user == null ? 0 : new_user;
+            userDailyList.add(new_user);
+            userTotal += new_user;
+            userTotalList.add(userTotal);
+        }
+
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .newUserList(StringUtils.join(userDailyList, ","))
+                .totalUserList(StringUtils.join(userTotalList, ","))
                 .build();
     }
 }
